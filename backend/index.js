9249731dc121a37daadcb84ad6bc5330702ba65f
Kauã Xavier <kauaxavier032@gmail.com>
2023-08-24 22:28:09 -0300

    feat: create backend initial and frontend initial

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const {v4: uuid} = require('uuid')
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+app.get('/user', (req, res) => {
+    const { email, password } = req.body;
+
+    const user = users.find((user) => user.password === password && user.email === email);
+
+    if(user){
+        return res.status(201).json(user);
+    } else{
+        return res.status(201).json('Esse usuário não existe.');
+    }
+
+});
+
+app.get('/users', (req, res) => {
+    return res.status(201).json(users);
+});
+
+app.post('/createaccount', (req, res) => {
+    const { name, email, password} = req.body;
+
+    const user = {
+        id : uuid(),
+        name : name,
+        email : email,
+        password : password
+    };
+
+    users.push(user);
+
+    return res.status(201).json(user);
+});
+
+app.listen(PORT, () =>{
+    console.log(`Server ON 🟢 ${PORT}`);
+});
